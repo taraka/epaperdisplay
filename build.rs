79f13a8a7948a0e1411de897271097ee3ebbe84d
TaRaKa <git@tomrawcliffe.co.uk>
2020-06-07 13:16:41 +0100

    running example

--- build.rs
+++ build.rs
@@ -2,25 +2,33 @@
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=lib/*.c");
+
+    //This doesn't seem to be working so added the lib manually for now
     println!("cargo:rustc-link-lib=wiringPi");
+
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
-        //.file("lib/EPD_7in5_V2.h")
-        //.file("lib/GUI_BMPfile.h")
-        //.file("lib/fonts.h")
+        .file("lib/DEV_Config.c")
+        .file("lib/dev_hardware_SPI.c")
+        .file("lib/RPI_sysfs_gpio.c")
+        .file("lib/ImageData.c")
+        .file("lib/GUI_Paint.c")
+        .file("lib/GUI_BMPfile.c")
+
         .file("lib/font12.c")
         .file("lib/font24CN.c")
-        //.file("lib/DEV_Config.h")
-        //.file("lib/GUI_Paint.h")
         .file("lib/font8.c")
         .file("lib/font20.c")
         .file("lib/font24.c")
         .file("lib/font16.c")
-        .file("lib/DEV_Config.c")
-        .file("lib/lib.c")
         .file("lib/font12CN.c")
-        .file("lib/GUI_Paint.c")
-        .file("lib/GUI_BMPfile.c")
         .file("lib/EPD_7in5_V2.c")
-            .compile("epd.a");
+        .file("lib/EPD_7in5_V2_test.c")
+
+        // This should be found by ld but not sure why it wasn't working
+        .object("/usr/lib/libwiringPi.so")
+
+        .define("USE_WIRINGPI_LIB", None)
+        .define("RPI", None)
+        .compile("epd.a");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern {
+    fn EPD_7in5_V2_test();
+}
+
 fn main() {
     println!("Hello, world!");
+    unsafe { EPD_7in5_V2_test() }
 }
