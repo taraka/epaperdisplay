3cbcd6f931a781e216b4047c1a852d731288cac2
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2020-06-17 19:49:45 +0100

    Removing ref to deleted files in build script

--- build.rs
+++ build.rs
@@ -5,25 +5,13 @@ fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=lib/*.c");
 
-    //This doesn't seem to be working so added the lib manually for now
-    println!("cargo:rustc-link-lib=wiringPi");
-
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
-        .file("lib/DEV_Config.c")
-        .file("lib/dev_hardware_SPI.c")
-        .file("lib/RPI_sysfs_gpio.c")
         .file("lib/font12.c")
         .file("lib/font8.c")
         .file("lib/font20.c")
         .file("lib/font24.c")
         .file("lib/font16.c")
-
-        // This should be found by ld but not sure why it wasn't working
-        .object("/usr/lib/libwiringPi.so")
-
-        .define("USE_WIRINGPI_LIB", None)
-        .define("RPI", None)
         .compile("epd.a");
 }
 
